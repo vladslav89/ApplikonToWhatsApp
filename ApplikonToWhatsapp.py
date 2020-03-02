ca4870979f960e06942203d44c745a2c65bf34d8
vladslav89 <vladslav89@gmail.com>
2020-03-02 10:51:35 +0300

    Now it extract data from Applikon db to analize processes for basic errors

--- ApplikonToWhatsapp.py
+++ ApplikonToWhatsapp.py
@@ -1,4 +1,5 @@
 import schedule
+import pyodbc
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -26,10 +27,16 @@ choice = "no"
 docChoice = "yes"
 doc_filename = "screen.bmp"
 
+process1 = 'GL0120_proc'
+process2 = 'TA0420'
+process3 = 'TA0520'
 
-
-
-
+server = 'tcp:' 
+database = 'biobase' 
+username = 'sa' 
+password = '' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 11 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
 
 
 def whatsapp_login():
@@ -174,6 +181,60 @@ def screenshot_sender():
     time.sleep(5)
     browser.close()
 
+
+def data_from_db(data_index):
+    cursor.execute('''SELECT TOP (30) [DPOINT_TIMESHIFT], [DPOINT_VALUE]
+                      FROM [Biobase].[dbo].[DATAPOINTS]
+                      WHERE DATA_INDEX = %s
+                      ORDER BY DPOINT_TIMESHIFT DESC''' % data_index)
+    rows = cursor.fetchall()
+    return([rows[0].DPOINT_VALUE, rows[2].DPOINT_VALUE, rows[4].DPOINT_VALUE, rows[14].DPOINT_VALUE, rows[29].DPOINT_VALUE, rows[0].DPOINT_TIMESHIFT])
+    #Возвращает значения на данное время, 3, 5, 15 и 30 минут назад, а также время         
+
+
+def data_check(process, pH_index, do2_index, temp_index, weight_index, o2_index, co2_index):
+    global message
+    massage = ''
+    ph = data_from_db(pH_index)
+    do2 = data_from_db(do2_index)
+    temp = data_from_db(temp_index)
+    weight = data_from_db(weight_index)
+    o2 = data_from_db(o2_index)
+    co2 = data_from_db(co2_index)
+    
+    new_time = ph[5]
+    if process[0:2] == 'GL' :
+        if abs(ph[0] - ph[4])>0.05 or abs(ph[0] - ph[3])>0.05 or abs(ph[0] - ph[2])>0.03:
+            message = 'Проблема с pH'
+        if do2[2] < 27 and do2[1] < 27 and do2[0] < 27:
+            message = 'Проблема с dO2'  
+        if (new_time < 576000000 and (abs(temp[0] - 37) > 0.5)) or (new_time > 622800000 and (abs(temp[0] - 32) > 0.5)) :
+            message = 'Проблема с температурой'
+        if abs(weight[0] - 100) > 8 or abs(weight[0] - weight[1]) > 1.5 or abs(weight[0] - weight[3]) > 1.5 or abs(weight[0] - weight[4]) > 1.5:
+            message = 'Проблема с весом'           
+        if abs(o2[0] - 0.25) > 0.2:
+            message = 'Проблема с кислородом' 
+        print(ph[0])
+
+    if process[0:2] == 'TA' :
+        new_time = ph[5]
+        if abs(ph[0] - ph[4])>0.05 or abs(ph[0] - ph[2])>0.03 or abs(ph[0] - 6.9)>0.05:
+            message = 'Проблема с pH'
+        if do2[2] < 27 and do2[1] < 27 and do2[0] < 27:
+            message = 'Проблема с dO2'
+        if (new_time < 403200000 and (abs(temp[0] - 37) > 0.5)) or (new_time > 453600000 and (abs(temp[0] - 32) > 0.5)) :
+            message = 'Проблема с температурой'       
+        if new_time > 403200000 and (abs(co2[0] - 0.5) > 0.45) :
+            message = 'Проблема с кислородом'   
+        print(weight[0])
+    if massage != '':
+        Contact = ['"' + 'Муж' + '"']
+        whatsapp_login()
+        sender()
+        browser.close()
+        time.sleep(5)
+        
+
     
 SUB100Sys1 = 0
 SUB100Sys2 = 0
@@ -182,10 +243,70 @@ SUB1000Sys2 = 0
 SUB1000Sys3 = 0
 time.sleep(10)
 
+cursor.execute('''SELECT  [DATA_VARIABLE_NAME], [DATA_INDEX]
+                  FROM [Biobase].[dbo].[DATAS] AS Datas JOIN [Biobase].[dbo].[PROCESSES] AS Process
+                  ON Datas.PROCESS_INDEX = Process.PROCESS_INDEX
+                  Where (Datas.LOGGING_MODE = 1) and (Process.PROCESS_NAME = '%s')''' % process1)
+rows = cursor.fetchall()
+for row in rows:
+    if row.DATA_VARIABLE_NAME == 'm_ph':
+        pH_index1 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_temp1':
+        temp_index1 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_do2':
+        do2_index1 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_weight':
+        weight_index1 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_o2flow_sparger':
+        o2_index1 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_co2flow_sparger':
+        co2_index1 = str(row.DATA_INDEX)
+
+cursor.execute('''SELECT  [DATA_VARIABLE_NAME], [DATA_INDEX]
+                  FROM [Biobase].[dbo].[DATAS] AS Datas JOIN [Biobase].[dbo].[PROCESSES] AS Process
+                  ON Datas.PROCESS_INDEX = Process.PROCESS_INDEX
+                  Where (Datas.LOGGING_MODE = 1) and (Process.PROCESS_NAME = '%s')''' % process2)
+rows = cursor.fetchall()
+for row in rows:
+    if row.DATA_VARIABLE_NAME == 'm_ph':
+        pH_index2 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_temp1':
+        temp_index2 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_do2':
+        do2_index2 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_weight':
+        weight_index2 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_o2flow_sparger':
+        o2_index2 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_co2flow_sparger':
+        co2_index2 = str(row.DATA_INDEX)
+
+cursor.execute('''SELECT  [DATA_VARIABLE_NAME], [DATA_INDEX]
+                  FROM [Biobase].[dbo].[DATAS] AS Datas JOIN [Biobase].[dbo].[PROCESSES] AS Process
+                  ON Datas.PROCESS_INDEX = Process.PROCESS_INDEX
+                  Where (Datas.LOGGING_MODE = 1) and (Process.PROCESS_NAME = '%s')''' % process3)
+rows = cursor.fetchall()
+for row in rows:
+    if row.DATA_VARIABLE_NAME == 'm_ph':
+        pH_index3 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_temp1':
+        temp_index3 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_do2':
+        do2_index3 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_weight':
+        weight_index3 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_o2flow_sparger':
+        o2_index3 = str(row.DATA_INDEX)
+    if row.DATA_VARIABLE_NAME == 'm_co2flow_sparger':
+        co2_index3 = str(row.DATA_INDEX)
+
 print("Web Page Open")
 print("SCAN YOUR QR CODE FOR WHATSAPP WEB")
 screenshot_sender()
 schedule.every(30).minutes.do(screenshot_sender,)
+schedule.every(2).minutes.do(data_check, process1, pH_index1, do2_index1, temp_index1, weight_index1, o2_index1, co2_index1)
+schedule.every(2).minutes.do(data_check, process2, pH_index2, do2_index2, temp_index2, weight_index2, o2_index2, co2_index2)
+schedule.every(2).minutes.do(data_check, process3, pH_index3, do2_index3, temp_index3, weight_index3, o2_index3, co2_index3)
 
 while True:
      
